bfa4646d61c8bd1a255ff7a32a662cfcf2156ab2
Cereal-Killa <pcfaguiar@gmail.com>
2021-06-04 14:49:54 -0300

    Basic support for start-up parameters, including start "minimized".

--- Program.cs
+++ Program.cs
@@ -15,13 +15,14 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 using OpenHardwareMonitor.GUI;
+using OpenHardwareMonitor.Utilities;
 
 namespace OpenHardwareMonitor {
   public static class Program {
-
+    public static StartupParams Arguments;
     [STAThread]
-    public static void Main() {
-      #if !DEBUG
+    public static void Main(string[] args) {
+#if !DEBUG
         Application.ThreadException += 
           new ThreadExceptionEventHandler(Application_ThreadException);
         Application.SetUnhandledExceptionMode(
@@ -29,11 +30,12 @@ namespace OpenHardwareMonitor {
 
         AppDomain.CurrentDomain.UnhandledException += 
           new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
-      #endif
+#endif
 
       if (!AllRequiredFilesAvailable() || !IsNetFramework45Installed())
         Environment.Exit(0);
 
+      Arguments = new StartupParams(args);
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       using (GUI.MainForm form = new GUI.MainForm()) {
@@ -49,12 +51,12 @@ namespace OpenHardwareMonitor {
         Path.DirectorySeparatorChar;
 
       if (!File.Exists(path + fileName)) {
-        MessageBox.Show("The following file could not be found: " + fileName + 
+        MessageBox.Show("The following file could not be found: " + fileName +
           "\nPlease extract all files from the archive.", "Error",
            MessageBoxButtons.OK, MessageBoxIcon.Error);
         return false;
       }
-      return true;      
+      return true;
     }
 
     private static bool AllRequiredFilesAvailable() {
@@ -66,12 +68,12 @@ namespace OpenHardwareMonitor {
         return false;
       if (!IsFileAvailable("OxyPlot.WindowsForms.dll"))
         return false;
-      
+
       return true;
     }
 
     private static bool IsNetFramework45Installed() {
-      Type type;      
+      Type type;
       try {
         type = TryGetDefaultDllImportSearchPathsAttributeType();
       } catch (TypeLoadException) {
@@ -96,9 +98,9 @@ namespace OpenHardwareMonitor {
       form.ShowDialog();
     }
 
-    public static void Application_ThreadException(object sender, 
-      ThreadExceptionEventArgs e) 
-    {
+    public static void Application_ThreadException(object sender,
+      ThreadExceptionEventArgs e)
+      {
       try {
         ReportException(e.Exception);
       } catch {
@@ -107,9 +109,9 @@ namespace OpenHardwareMonitor {
       }
     }
 
-    public static void CurrentDomain_UnhandledException(object sender, 
-      UnhandledExceptionEventArgs args) 
-    {
+    public static void CurrentDomain_UnhandledException(object sender,
+      UnhandledExceptionEventArgs args)
+      {
       try {
         Exception e = args.ExceptionObject as Exception;
         if (e != null)
@@ -118,6 +120,6 @@ namespace OpenHardwareMonitor {
       } finally {
         Environment.Exit(0);
       }
-    }   
+    }
   }
 }
---
+++ Utilities/StartupParams.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenHardwareMonitor.Utilities {
+  public class StartupParams {
+    private List<Types> Parameters;
+
+    public StartupParams(string[] args) {
+      Parameters = new List<Types>();
+
+      for (int i = 0; i < args.Length; i++) {
+        Types type;
+
+        if (!args[i].StartsWith("--"))
+          continue;
+
+        if (Enum.TryParse(args[i].ToUpper().TrimStart('-'), out type)) {
+          Parameters.Add(type);
+        }
+      }//for
+    }
+
+    public bool Contains(Types type) {
+      return Parameters.Contains(type);
+    }
+
+    public enum Types {
+      UNKNOWN,
+      STARTMINIMIZED
+    }
+  }
+}
